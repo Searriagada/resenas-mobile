fbcd82ac0b90e276e117cc8c8291dec8cb5e6d08
VictorSilva <victor.silva.pradenas@ciisa.cl>
2024-11-09 18:50:10 -0300

    se crean rutas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { createOutline, personAddOutline, videocam, listOutline } from 'ionicons/icons';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonTab, IonTabBar, IonTabButton, IonIcon, IonTabs } from '@ionic/angular/standalone';
+import{Router} from '@angular/router'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonTab, IonTabBar, IonTabButton, IonIcon, IonTabs],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private router:Router) {
+    addIcons({ createOutline, personAddOutline, videocam, listOutline });
+  }
+  listarPelicula(){
+    this.router.navigate(['/listar-pelicula'])
+  }
+
+  nuevoUsuario(){
+    this.router.navigate(['/agregar-usuario'])
+  }
+
+  nuevaPelicula(){
+    this.router.navigate(['/agregar-pelicula'])
+  }
+
+  editarPelicula(){
+    this.router.navigate(['/editar-pelicula'])
+  }
 }
